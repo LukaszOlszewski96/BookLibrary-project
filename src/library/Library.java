98d4a3d610ef7a7ea091506392158d5062abbd89
LukaszOlszewski96 <57043892+LukaszOlszewski96@users.noreply.github.com>
2019-11-18 15:31:01 +0100

    add DB navigator with tables

--- src/library/Library.java
+++ src/library/Library.java
@@ -2,6 +2,9 @@ package library;
 import java.sql.*;
 
 public class Library {
+    public static void main(String[] args) {
+
+    }
     public static final String DB_URL = "jdbc:mysql://localhost:3306/librasydb";
     public static final String DRIVER = "com.mysql.jdbc.Driver";
     private Connection connection = null;
@@ -14,7 +17,6 @@ public class Library {
             System.err.println("Driver not found");
             e.printStackTrace();
         }
-
         try {
             connection = DriverManager.getConnection(DB_URL);
             statement = connection.createStatement();
@@ -22,9 +24,5 @@ public class Library {
             System.err.println("Problem with connection");
             e.printStackTrace();
         }
-        createTables();
-    }
-    public boolean createTables(){
-        String createBooks = "CREATE TABLE IF NOT EXISTS books (id_book INTEGER PRIMARY KEY AUTOINCREMENT,   )";
     }
 }
